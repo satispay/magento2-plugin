e95786bd6483657bf12975af70f292ec3769a93e
Enrico Naletto <en4letto@gmail.com>
2020-06-10 12:34:30 +0200

    Cancel order if payment was not accepted

--- Controller/Redirect/Index.php
+++ Controller/Redirect/Index.php
@@ -1,17 +1,22 @@
 <?php
 namespace Satispay\Satispay\Controller\Redirect;
 
+
 class Index extends \Magento\Framework\App\Action\Action
 {
     protected $checkoutSession;
 
+    protected $orderRepository;
+
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Checkout\Model\Session $checkoutSession,
-        \Satispay\Satispay\Model\Method\Satispay $satispay
+        \Satispay\Satispay\Model\Method\Satispay $satispay,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
     ) {
         parent::__construct($context);
         $this->checkoutSession = $checkoutSession;
+        $this->orderRepository = $orderRepository;
     }
 
     public function execute()
@@ -21,6 +26,9 @@ class Index extends \Magento\Framework\App\Action\Action
         if ($satispayPayment->status == 'ACCEPTED') {
             $this->_redirect('checkout/onepage/success');
         } else {
+            $order = $this->checkoutSession->getLastRealOrder();
+            $order->registerCancellation(__('Payment has been cancelled.'));
+            $this->orderRepository->save($order);
             $this->checkoutSession->restoreQuote();
             $this->_redirect('checkout/cart');
         }
