ef9f70b51e7f3776e45a7969b57b91ce2a6a7412
MichaelBattistonBitbull <michael.battiston@bitbull.it>
2022-04-07 17:27:44 +0200

    Add logs

--- Controller/Payment/Index.php
+++ Controller/Payment/Index.php
@@ -6,17 +6,20 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     protected $checkoutSession;
     protected $orderRepository;
+    protected $logger;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Checkout\Model\Session $checkoutSession,
         \Satispay\Satispay\Model\Method\Satispay $satispay,
-        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        \Psr\Log\LoggerInterface $logger
     )
     {
         parent::__construct($context);
         $this->checkoutSession = $checkoutSession;
         $this->orderRepository = $orderRepository;
+        $this->logger = $logger;
     }
 
     public function execute()
@@ -42,6 +45,8 @@ class Index extends \Magento\Framework\App\Action\Action
                 // Set last transition id as the satispay payment id
                 $payment->setLastTransId($satispayPayment->id);
                 $this->orderRepository->save($order);
+            } else {
+                $this->logger->critical("Satispay - Couldn't save transaction id for order: " . $order->getId());
             }
             $this->_redirect($satispayPayment->redirect_url);
         }
